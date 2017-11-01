fdcaad4979549e6f7f00525f0a89b5cd2fb9dbb8
Carl Manganelli <cjmanganelli@gmail.com>
2017-11-01 14:02:52 -0500

    started work on form checking for the register page

---
+++ javascript/formCheck.js
@@ -0,0 +1,54 @@
+function fromCheck(str1, str2) {
+    
+    console.log(str);
+    
+    //document.getElementById("passMatch").innerHTML = '<p>Loading:<i class="fa fa-spinner fa-spin"</p>';
+    
+    xmlhttp = new XMLHttpRequest();
+    
+    xmlhttp.onreadystatechange = function() {
+        
+      if(this.readyState == 4 && this.status == 200) {
+          
+        $response = this.responseText;
+          
+        /*
+            -php service will check for:
+                1. Both passwords match
+                2. password meets password requirements
+                
+            -then return different strings
+                1. 'accepted': text fields match and meets the password requirements
+                2. 'noMatch': text fields don't match
+                3. 'noReqs': password does not meet the requirements
+        
+        */
+          
+        if($response == 'accepted') {
+            document.getElementById("registerBtn").className="btn btn-default";
+            document.getElementById("passMatch").innerHTML = '';
+        }
+        else if ($response == 'noMatch'){
+            document.getElementById("registerBtn").className="btn btn-default disabled";
+            document.getElementById("passMatch").innerHTML = '<p>Passwords do not match</p>';
+            
+        }
+        else if ($response == 'noReqs') {
+            document.getElementById("registerBtn").className="btn btn-default disabled";
+            document.getElementById("passMatch").innerHTML = '<p>Password must be 8-10 characters long.</p>';
+            
+        }
+        else {
+            document.getElementById("registerBtn").className="btn btn-default disabled";
+            document.getElementById("passMatch").innerHTML = '<p>Should Not Be Here</p>';
+        }
+          
+         
+      }
+
+    };
+    xmlhttp.open("POST", "./phpservices/formCheck.php?pass=" + str1 + "&check=" + str2, true);
+    xmlhttp.send();
+      
+}
+
--- login.php
+++ login.php
@@ -8,7 +8,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>GameOn-MU</title>
+    <title>Sign In!</title>
 
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
