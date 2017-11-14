8820e970ea9c01384fec4613d872dfab55ac971a
Carl Manganelli <cjmanganelli@gmail.com>
2017-11-14 10:43:05 -0600

    added count me in handler

---
+++ phpservices/countMeIn.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+?>
+
+
+
+
+<form role="form" action="./phpservices/countMeIn.php" method="POST">
+
+    <input type="hidden" name="eventID" value="">
+    <input>
+
+
+
+
+
+
+
+
+</form>
